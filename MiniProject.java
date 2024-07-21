87efa0769607661bab6c0b79e2bf4156c3a3889c
Shadab Akhtar <142724587+shadabakhtar08@users.noreply.github.com>
2024-07-21 16:50:46 +0530

    Add files via upload

---
+++ MiniProject.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MiniProject {
+    public static void main(String[] args) {
+        // MINI PROJECT
+        Scanner sc =new Scanner(System.in);
+        int myNumber = (int)(Math.random()*100);
+        int userNumber = 0;
+
+        do{
+            System.out.println("Guess my number (1-100): ");
+            userNumber = sc.nextInt();
+
+            if (userNumber == myNumber) {
+                System.out.println("woohoo .. CORRECT NUMBER!!! ");
+                break;
+            }
+            else if (userNumber > myNumber) {
+                System.out.println("Your number is too large ");
+            }
+            else{
+                System.out.println("Your number is too small");
+            }
+        }
+        while(userNumber >= 0);
+
+        System.out.println("My number was: " + myNumber);
+
+    }
+
+}
